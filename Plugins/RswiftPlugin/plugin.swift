ed137230b537462797a6d0d37873dcb4d1461b27
Nicolai Harbo <niha@trifork.com>
2022-10-27 14:02:16 +0200

    downgraded r.swift version

--- Plugins/RswiftPlugin/plugin.swift
+++ Plugins/RswiftPlugin/plugin.swift
@@ -13,10 +13,8 @@ import Foundation
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
         let outputPath = context.package.directory.appending(["Sources", "FalconTheme"])
         
-        let (sdkPath, _) = shell("xcrun", "--sdk", "macosx", "--show-sdk-path")
-        
         return [
-            .buildCommand(
+            .prebuildCommand(
                 displayName: "Running R.swift",
                 executable: try context.tool(named: "rswift").path,
                 arguments: [
@@ -25,38 +23,8 @@ import Foundation
                     "--target", target.name,
                     "--accessLevel", "public"
                 ],
-                environment: [
-                    "SDKROOT": sdkPath ?? "",
-                ],
-                inputFiles: [],
-                outputFiles: []
+                outputFilesDirectory: outputPath
             )
         ]
-        
-        //        return [.prebuildCommand(
-        //            displayName: "Running R.swift",
-        //            executable: try context.tool(named: "rswift").path,
-        //            arguments: [
-        //                "generate", outputPath.appending("R.generated.swift"),
-        //                "--swiftPackage", context.package.directory.string,
-        //                "--target", target.name,
-        //                "--accessLevel", "public"
-        //            ],
-        //            outputFilesDirectory: outputPath)]
     }
-}
-
-@discardableResult
-func shell(_ args: String...) -> (String?, Int32) {
-    let task = Process()
-    task.launchPath = "/usr/bin/env"
-    task.arguments = args
-    let pipe = Pipe()
-    task.standardOutput = pipe
-    task.standardError = pipe
-    task.launch()
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)
-    task.waitUntilExit()
-    return (output, task.terminationStatus)
 }
